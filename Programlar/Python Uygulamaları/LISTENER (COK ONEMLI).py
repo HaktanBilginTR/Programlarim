a46eda3c531fb3ec601ff8f440e941d3633a536d
HaktanBilginTR <42042313+HaktanBilginTR@users.noreply.github.com>
2019-03-04 23:51:27 +0300

    Add files via upload

--- Programlar/Python Uygulamaları/LISTENER (COK ONEMLI).py
+++ Programlar/Python Uygulamaları/LISTENER (COK ONEMLI).py
@@ -1,7 +1,16 @@
 from pynput.keyboard import Key, Listener
 evennts = []
+
+
 def on_press(Key):
     key_press = Key
     evennts.append(key_press)
+    if str(Key) == "'a'":
+        return print("anne")
+    elif str(Key) == "'b'":
+        return print("baba")
+    return print("haha")
+
+
 with Listener(on_press=on_press) as listener:
     listener.join()
